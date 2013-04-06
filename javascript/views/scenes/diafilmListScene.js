d0d3dc0014bf7bb04e6951e6630e4a2c6d280ff9
Dmitry Ryumin <dmitryryumin@Dmitry-Ryumins-MacBook-Pro.local>
2013-04-06 21:13:57 +0700

    tiles

--- javascript/views/scenes/diafilmListScene.js
+++ javascript/views/scenes/diafilmListScene.js
@@ -8,16 +8,50 @@
             this.collection = this.options.diafilmCollection;
 
             this.diafilms = new app.widgets.tilesWidget({
-                tileWidth: 100,
-                tileHeight: 100
+                tileWidth: 160,
+                tileHeight: 160
             });
-            this.diafilms.sourceDelegate = this;
+            this.diafilms.getTileOnIndex = function(index){
+                var item = _this.collection[index];
+
+                var imgSrc = _this.collection[index].get('img');
+                var parts = imgSrc.match(/^http:\/\/diafilmy.su\/(.*)$/);
+
+                var imgUrl = "http://diafilmy.su/phpthumb/phpThumb.php?src=/"+parts[1]+"&w=160&h=160&zc=1";
+
+                var html = $(
+                    '<div>'+
+                        '<div class="opacity"></div>'+
+                        '<img width="160" src="'+imgUrl+'"/>'+
+                        '<div class="title">'+item.get('title')+'</div>'+
+                    '</div>'
+                );
+
+                /*var img = document.createElement("img");
+                img.src = imgSrc;
+                //img.width = 160;
+
+                var html = img;*/
+                return html;
+            };
 
             this.addWidget(this.diafilms);
 
             app.log('COUNT', _this.collection.length)
             _this.diafilms.count = _this.collection.length;
             _this.render();
+
+            /*var loadedImg = 0;
+            for(var i = 0; i < 100; i++){
+                $('<img>').attr('src', this.collection[i].get('img')).load(function(){
+                    app.log('loaded')
+                    loadedImg++;
+                    if(loadedImg >= 100){
+                        app.log('LOADED!');
+                        _this.render();
+                    }
+                });
+            }*/
 
             this.diafilms.on("key_enter", function(){
                  app.showScene("diafilmScene", {
@@ -26,11 +60,6 @@
              });
 
             this.focusWidget(this.diafilms);
-        },
-        getItemOnIndex: function(index){
-            //var item = this.collection.at(index);
-            var item = this.collection[index];
-            return item.get('title');
         },
         render: function(){
             $(this.el).html(new EJS({url: 'javascript/templates/diafilmList.ejs'}).render({
--- javascript/views/widgets/tilesWidget.js
+++ javascript/views/widgets/tilesWidget.js
@@ -1,20 +1,111 @@
 
 (function(){
     app.widgets.tilesWidget = app.Widget.extend({
+        getTileOnIndex: function(index){
+            return 'Tile ' + index;
+        },
         initialize: function(){
-            this.count = 50;
 
-            this.tileWidth = this.options.tileWidth;
-            this.tileHeight = this.options.tileHeight;
+            this.cache = [];
 
+            this.tileWidth = this.options.tileWidth || this.tileWidth;
+            this.tileHeight = this.options.tileHeight || this.tileHeight;
+
+            this.activeTileIndex = 0;
+
+            this.calculateCount();
             this.prepareTiles();
+
+            this.on("key_right", this.right, this);
+            this.on("key_left", this.left, this);
         },
         //options
         count: 0,
         tileWidth: 206,
         tileHeight: 192,
-        rows: 0,
+        tileMargin: 5,
+        rows: 3,
+        containerWidth: 1280,
 
+        shift: 0,
+
+        cols: 0,
+
+        outRightTileIndex: 0,
+        outRightVal: 0,
+
+        right: function(){
+            app.log('slides right')
+
+            if(this.activeTileIndex == this.outRightTileIndex - 1){
+                this.shift++;
+                this.renderTiles();
+
+                //var margin = this.firstMargin;//- (this.tileWidth / 4 - (this.tileWidth - this.outRightVal));
+                //app.log('MARGIN: ', margin)
+                //this.wrapper.css('margin-left', margin+'px');
+            }
+            else{
+                this.activateTileOnIndex(this.activeTileIndex + 1);
+            }
+
+
+        },
+        left: function(){
+            app.log('slides left')
+
+            this.activateTileOnIndex(this.activeTileIndex - 1);
+        },
+
+        focus: function(){
+            app.log('tiles focus')
+
+            this.activateTileOnIndex(this.activeTileIndex);
+        },
+        blur: function(){
+            app.log('tiles blur')
+        },
+
+        activateTileOnIndex: function(index){
+            this.deactivateTileOnIndex(this.activeTileIndex);
+            this.activateTile(this.tiles[index]);
+            this.activeTileIndex = index;
+        },
+        deactivateTileOnIndex: function(index){
+            this.deactivateTile(this.tiles[index]);
+        },
+        activateTile: function(tile){
+            var el = tile.el;
+
+            el.addClass('pseudo-hover');
+        },
+        deactivateTile: function(tile){
+            var el = tile.el;
+
+            el.removeClass('pseudo-hover');
+        },
+
+        calculateCount: function(){
+            var w = 0;
+            for(var i = 0; true; i++){
+                w += this.tileWidth + this.tileMargin * 2;
+                if(w > this.containerWidth){
+                    this.cols = i + 1;
+                    if(w - this.containerWidth < this.tileWidth/2){
+                        this.cols++;
+                    }
+                    this.count = this.cols * this.rows;
+
+                    this.outRightTileIndex = i;
+                    this.outRightVal = w - this.containerWidth;
+
+                    app.log('COUNT:', this.count)
+                    app.log('COLS:', this.cols)
+                    app.log('Outed:', this.outRightTileIndex, 'OutedVal:', this.outRightVal)
+                    break;
+                }
+            }
+        },
         prepareTiles: function(){
             this.tiles = [];//{el: ..., widget: ...}
 
@@ -25,8 +116,14 @@
 
             this.wrapper = this.tpl;
 
-            //this.wrapper.css();
+            var wrapperHeight = (this.tileHeight + this.tileMargin*2) * this.rows;
 
+            app.log('wrapper height', wrapperHeight)
+
+            this.wrapper.css('height', wrapperHeight + 'px');
+
+            var w = 0;
+            var outedRight = false;
             for(var i = 0; i < this.count; i++){
                 var tileEl = $('<div class="tile">');
 
@@ -37,13 +134,58 @@
 
                 tileEl.text(i);
                 this.wrapper.append(tileEl);
+
+                var row = Math.floor(i / this.cols);
+                var col = i % this.cols;
+
+                var index = col * this.rows + row;
+
+                var tile = {
+                    el: tileEl,
+                    i: i,
+                    index: index
+                };
+
+                this.tiles.push(tile);
+
             }
 
+            var wrapperWidth = (this.tileWidth + this.tileMargin*2) * this.cols;
+            app.log('wrapper width', wrapperWidth)
+            this.wrapper.css('width', wrapperWidth + 'px');
+
+        },
+        renderTiles: function(){
+            var _this = this;
+            _.each(this.tiles, function(tile, i){
+                var tileEl = tile.el;
+                var index = tile.index + _this.shift * _this.rows;
+
+                if(!_this.cache[index]){
+                    _this.cache[index] = _this.getTileOnIndex(index);
+                }
+
+                var html = _this.cache[index];
+
+                if(app.config.debug){
+                    var debug = $('<div class="tile-debug">').text(index).appendTo(html);
+                }
+
+                tileEl.html(html);
+                /*var el = tileEl.get(0);
+                if(el.firstChild){
+                    var a = el.removeChild(el.firstChild);
+                }
+
+                el.appendChild(html)*/
+            });
         },
         tagName: 'div',
         className: 'tiles',
         render: function(){
             this.$el.html(this.tpl);
+
+            this.renderTiles();
 
             return this;
         }
